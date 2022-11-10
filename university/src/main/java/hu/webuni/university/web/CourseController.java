15631887bbd92cd0096a9f2d7e250ed6985cd766
imre-gabor <gabor@aut.bme.hu>
2022-11-10 18:53:15 +0100

    week 4: add cancelLesson

--- university/src/main/java/hu/webuni/university/web/CourseController.java
+++ university/src/main/java/hu/webuni/university/web/CourseController.java
@@ -1,5 +1,6 @@
 package hu.webuni.university.web;
 
+import java.time.LocalDate;
 import java.time.OffsetDateTime;
 import java.util.List;
 import java.util.Optional;
@@ -10,9 +11,12 @@ import javax.validation.constraints.NotNull;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.querydsl.binding.QuerydslPredicate;
 import org.springframework.data.web.SortDefault;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.server.ResponseStatusException;
 
 import com.querydsl.core.types.Predicate;
 
@@ -34,7 +38,9 @@ public class CourseController implements CourseControllerApi {
 	
 	private final CourseMapper courseMapper;
 	private final NativeWebRequest request;
-	private final MethodArgumentResolverHelper resolverHelper;	
+	private final MethodArgumentResolverHelper resolverHelper;
+	
+	private final SimpMessagingTemplate simpMessagingTemplate;
 
 	@Override
 	public Optional<NativeWebRequest> getRequest() {
@@ -87,7 +93,18 @@ public class CourseController implements CourseControllerApi {
 			Iterable<Course> courses = courseRepository.findAll(predicate, pageable);
 			return ResponseEntity.ok(courseMapper.courseSummariesToDtos(courses));
 		}
+	}
+
+	@Override
+	public ResponseEntity<Void> cancelLesson(Integer courseId, LocalDate day) {
+		Course course = courseRepository.findById(courseId).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+		simpMessagingTemplate.convertAndSend("/topic/courseChat/" + course.getId(),
+				String.format("A %s kurzus %s napon elmarad.", course.getName(), day));
+		
+		return ResponseEntity.ok().build();
 	}		
+	
+	
 	
 	
 }
